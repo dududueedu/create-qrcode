2028aba9f90a61812abf1af353e8115b6011db25
Eduardo Gomes <eduardooffside@gmail.com>
2023-11-08 16:11:45 -0300

    add function download qrcode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import QRCode from 'react-qr-code';
 import { useState } from 'react';
+import QrCodeCreate from 'qrcode'
 import './App.css';
 
 function App() {
   
   const [link, setLink] = useState('');
+  const [qrcode, setQrcode] = useState('');
   
+  function handleCreate(url_link){
+    QrCodeCreate.toDataURL(url_link, {
+      width: 600, 
+      margin: 2,
+    }, function (error, url){
+      setQrcode(url)
+    })
+  }
+
+  function handleQrcode(event){
+    setLink(event.target.value)
+    handleCreate(event.target.value)
+  }
+
   return (
     <div className="container">
 
@@ -14,8 +30,12 @@ function App() {
       <input className='input'
         placeholder='Digite seu link'
         value={link}
-        onChange={(event) => setLink(event.target.value)}
-        />
+        onChange={(event) => handleQrcode(event)}
+      />
+
+      <button className='btn'> 
+        <a className='tagA' href={qrcode} download={`meu-QRcode.png`}> Baixe aqui seu QRCode</a> 
+      </button>
     </div>
   );
 }
