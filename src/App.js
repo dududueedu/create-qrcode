5295ab60c1323230f771cf689dbc5ab6a65392e4
Eduardo Gomes <eduardooffside@gmail.com>
2023-11-08 10:57:13 -0300

    add qr-code with link

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import QRCode from 'react-qr-code';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  
+  const [link, setLink] = useState('');
+  
   return (
     <div className="container">
+
+      <QRCode value={link}/>
+      
       <input className='input'
-        placeholder='Digite seu link'/>
+        placeholder='Digite seu link'
+        value={link}
+        onChange={(event) => setLink(event.target.value)}
+        />
     </div>
   );
 }
